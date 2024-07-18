34b011dacc9004583e23b78c3cb6bedfb06743ab
alex-web-dev <alex.web-dev@mail.ru>
2024-07-18 12:40:46 +0300

    fix: corrections (17.07)

--- src/assets/js/mobile-menu.js
+++ src/assets/js/mobile-menu.js
@@ -2,9 +2,15 @@ const $menu = document.querySelector(".mobile-menu");
 if ($menu) {
   const $menuToggle = document.querySelector(".toggle-menu");
   $menuToggle.addEventListener("click", () => {
-    $menu.classList.toggle("mobile-menu--active");
-    $menuToggle.classList.toggle("toggle-menu--active");
-    document.body.classList.toggle("body--lock");
+    if ($menu.classList.contains('mobile-menu--active')) {
+      $menu.classList.remove("mobile-menu--active");
+      $menuToggle.classList.remove("toggle-menu--active");
+      document.body.classList.remove("body--lock");
+    } else {
+      $menu.classList.add("mobile-menu--active");
+      $menuToggle.classList.add("toggle-menu--active");
+      document.body.classList.add("body--lock");
+    }
   });
 
   $menu.addEventListener("click", (e) => {
